56caf528a6b1e2e9af208641fd5980c4940c61ad
Doni Crosby <doni.crosby1995@gmail.com>
2019-02-06 16:04:54 -0500

    Check that users supplied an email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,3 +29,8 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, fake.password())
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Tests creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, fake.password())
